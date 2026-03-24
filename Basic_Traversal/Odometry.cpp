1398718aa49d848ec4ceb34350e4dc328917ec36
BradyDCabral <24crl3@queensu.ca>
2026-03-24 00:17:28 -0400

    adjust hierarchy

---
+++ Basic_Traversal/Odometry.cpp
@@ -0,0 +1,20 @@
+#include "Odometry.h"
+
+
+
+
+
+
+
+
+int UpdateYAW(float Gz, float *zR, float *oR, float Dt) {
+  // if stationary just dont update YAW
+  Gz += ((std::abs)(Gz) > rZ_DEADZONE ? 0 : DRIFT_rZ);
+  float dz = (*oR + Gz)/2 * Dt;
+
+  *zR += (dz > -rZ_DEADZONE && dz < rZ_DEADZONE ? 0 : dz);
+  
+  *oR = Gz;
+  return SUCCESS;
+
+}
